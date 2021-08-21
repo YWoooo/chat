9593de68f335541ec7ad4d0540d0f1c691648344
YuweiChang <ywchangsmail@gmail.com>
2021-08-22 04:42:14 +0800

    feat: add /

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,10 @@ const io = new Server(server, {
   },
 })
 
+app.get('/', (req, res) => {
+  res.send('<h1>Hello world</h1>')
+})
+
 io.on('connection', socket => {
   console.log('a user connected')
 
